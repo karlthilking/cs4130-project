87fd7f9887a09e56c54117f6fefa95b29abf1e8c
Karl Thilking <thilking.k@northeastern.edu>
2025-12-04 13:49:19 -0500

    model class with llama-3.3-70b

---
+++ project_test.py
@@ -0,0 +1,171 @@
+from groq import Groq
+from typing import Union, Optional, Any
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os 
+
+class Model:
+  def __init__(self):
+    self.client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
+    self.model_name = "llama-3.3-70b-versatile"
+    self.datasets = {
+      "Electric Vehicles": "Data/electric_vehicles_spec_2025.csv",
+      "Iris Flowers": "Data/Iris.csv"
+    }
+    self.messages = [];
+  
+  def csv_to_df(self, csv_path: str) -> pd.DataFrame:
+    df = pd.read_csv(csv_path)
+    return df
+  
+  def load_dataset(self, df_name: str) -> Union[pd.DataFrame, None]:
+    csv_path = self.datasets.get(df_name, None)
+    if csv_path is None:
+      return None
+    else:
+      return pd.read_csv(csv_path)
+
+  def generate_code(self, df: pd.DataFrame, df_name: str, request: str) -> Union[str, None]:
+    prompt = f'''Generate Python code for data visualization.
+
+    Dataset name: {df_name}
+    Dataset categories: {', '.join(list(df.columns))}
+    User request: {request}
+
+    Your task:
+      1. Write a function, "def plot()", to generate a plot according to user request using matplotlib.
+      2. Write a second function, "def stats()", that returns a pandas DataFrame including stats about the specific data categories the user is interested in.
+      3. Use the variable "df" in your functions to access the {df_name} dataset which has the categories listed previously.
+    
+    Mandatory Rules:
+      1. Do not import any modules or libraries. All necessary libraries are already imported globally.
+      2. Do not create new data - use "df" to access all necessary data.
+      3. Do not include parameters in either function - you have global access to all variables and modules.
+      4. No comments or explanations, only write code.
+      5. Do not explicitly call either of your functions to execute them after writing their implementations. The user will invoke each functions on their own.
+      6. Do not use plt.show(). The user will display the graph on their own.
+
+    Here is an example outline to follow:
+    def plot():
+      plt.plot(df['column_1'], df['column_2'])
+      plt.xlabel('column_1')
+      plt.ylabel('column_2')
+    
+    def stats():
+      return pd.DataFrame({{
+        'mean_column_1': [df['column_1'].mean()],
+        'mean_column_2': [df['column_2'].mean()]
+      }})
+    
+    Request: {request}
+    Write your functions here:'''
+    try:
+      response = self.client.chat.completions.create(
+        messages=[{
+          "role": "user", "content": prompt
+        }],
+        model=self.model_name,
+        max_tokens=350,
+        temperature=0.0
+      )
+      code = response.choices[0].message.content.replace('```python', '').replace('```', '').strip()
+      
+      self.messages = [
+        {"role": "user", "content": prompt},
+        response.choices[0].message
+      ]
+      return code
+    except Exception as e:
+      print(f'Error: {e}')
+  
+  def execute_code(self, code, df) -> Union[pd.DataFrame, None, Any]:
+    locals = {}
+    globals = {
+      'plt': plt,
+      'pd': pd,
+      'np': np,
+      'sns': sns,
+      'df': df,
+      '__builtins__': __builtins__
+    }
+    try:
+      exec(code, globals, locals)
+      if 'plot' in locals and callable(locals['plot']):
+        plt.close('all')
+        plt.figure(figsize=(10, 6))
+        locals['plot']()
+      else:
+        print('Failed to find plot function.\n')
+      if 'stats' in locals and callable(locals['stats']):
+        stats = locals['stats']()
+        if stats.empty:
+          return None
+        if len(list(stats.columns)) > 5:
+          stats = stats.iloc[:, :5]
+        return stats
+      else:
+        print('Failed to find stats function.\n')
+        return None
+    except SyntaxError as e:
+      print(f'Syntax Error: {e}.\n')
+    except Exception as e:
+      print(f'Error: {e}.\n')
+    
+  def generate_summary(self, df, df_name, request) -> Union[str, None]:
+    prompt=f'''You just generated a plot and stats for the user, now follow up with an explanation.
+    Explain important trends, relationships, correlations, etc. related to the findings from the plot and stat calculations. Keep your response to a very brief paragraph, only highlighting the most important details in order to be concise. Here was the user request: {request}'''
+
+    try:
+      response = self.client.chat.completions.create(
+        messages=[{"role": "user", "content": prompt}],
+        model=self.model_name,
+        max_tokens=200,
+        temperature=0.2
+      ).choices[0].message.content
+      return response
+    except Exception as e:
+      print(f'Error during summary generation: {e}')
+      return None
+  
+  def main(self):
+    df, df_name = None, None
+    while df is None:
+      datasets = [f'{i+1}. {k}' for i, k in enumerate(self.datasets.keys())]
+      choice = input(f"\nSelect a dataset from the following: {' '.join(datasets)} (Press 1 for 1st dataset, 2 for 2nd, etc.):\n")
+      if not choice.isdigit():
+        print(f'Your selection could not be identified. Please try again.\n')
+        continue
+      choice = int(choice)
+      if choice == 1 or choice == 2:
+        df_name = list(self.datasets.keys())[choice - 1]
+        df = self.csv_to_df(self.datasets[df_name])
+        print(f"You've selected the {df_name} dataset!\n")
+      else:
+        print(f'Selection out of range. Pleas try again.\n')
+    
+    print(f'Here are the categories within your dataset:\n{", ".join(list(df.columns))}\n')
+    while True:
+      request = input("Enter any data categories you are interested in and a plot type for visualization (press q to exit):\n")
+      if request.lower() == 'q':
+        break
+
+      code = self.generate_code(df, df_name, request)
+      stats = self.execute_code(code, df)
+
+      if stats is not None:
+        print(f'Here are some key statistics:\n{stats}\n')
+      else:
+        print('Failed to generate stats.\n')
+      
+      summary = self.generate_summary(df, df_name, request)
+      if summary:
+        print(f'Summary:\n{summary}\n')
+
+if __name__ == "__main__":
+  model = Model()
+  model.main()
+
+
+  
